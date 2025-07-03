99867721dde1d344777f64249e33381c8210c822
LiubovTaran <147322419+LiubovTaran@users.noreply.github.com>
2025-07-03 14:36:41 +0300

    06_change_over_time_analysis.sql

--- 06_change_over_time_analysis.sql
+++ 06_change_over_time_analysis.sql
@@ -21,7 +21,7 @@ SELECT
     SUM(sales_amount) AS total_sales,
     COUNT(DISTINCT customer_key) AS total_customers,
     SUM(quantity) AS total_quantity
-FROM gold.fact_sales
+FROM fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY YEAR(order_date), MONTH(order_date)
 ORDER BY YEAR(order_date), MONTH(order_date);
@@ -32,7 +32,7 @@ SELECT
     SUM(sales_amount) AS total_sales,
     COUNT(DISTINCT customer_key) AS total_customers,
     SUM(quantity) AS total_quantity
-FROM gold.fact_sales
+FROM fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(month, order_date)
 ORDER BY DATETRUNC(month, order_date);
@@ -43,7 +43,7 @@ SELECT
     SUM(sales_amount) AS total_sales,
     COUNT(DISTINCT customer_key) AS total_customers,
     SUM(quantity) AS total_quantity
-FROM gold.fact_sales
+FROM fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM');
